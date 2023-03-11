7bbe837f1fa85cea6d247594e46d73b3d4064299
Gabriel Borel <biel_borel@hotmail.com>
2023-03-11 19:39:10 -0300

    chore: register user service response interface

--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -1,24 +1,37 @@
 import { IUsersRepository } from '@/repositories/users-repository.interface';
+import { User } from '@prisma/client';
 import { hash } from 'bcryptjs';
 import { UserAlreadyExistsError } from './errors/user-already-exists.error';
 
-interface IRegisterUserServicePayload {
+interface IRegisterUserServiceRequest {
   name: string;
   email: string;
   password: string;
 }
 
+interface IRegisterUserServiceResponse {
+  user: User;
+}
+
 export class RegisterUserService {
   constructor(private usersRepository: IUsersRepository) {}
 
-  async execute(payload: IRegisterUserServicePayload) {
-    const { name, password, email } = payload;
+  async execute(
+    request: IRegisterUserServiceRequest
+  ): Promise<IRegisterUserServiceResponse> {
+    const { name, password, email } = request;
     const password_hash = await hash(password, 6);
 
     const emailAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (emailAlreadyExists) throw new UserAlreadyExistsError();
 
-    await this.usersRepository.create({ email, name, password_hash });
+    const user = await this.usersRepository.create({
+      email,
+      name,
+      password_hash,
+    });
+
+    return { user };
   }
 }
