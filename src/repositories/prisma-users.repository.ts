d6db446a5bd4250fb9bffdd8b743ff91ec42674f
Gabriel Borel <biel_borel@hotmail.com>
2023-03-11 18:04:29 -0300

    feat: users repository

---
+++ src/repositories/prisma-users.repository.ts
@@ -0,0 +1,13 @@
+import { prismaClient } from '@/lib/prisma';
+import { Prisma } from '@prisma/client';
+import { IUsersRepository } from './users-repository.interface';
+
+export class PrismaUsersRepository implements IUsersRepository {
+  async create(data: Prisma.UserCreateInput) {
+    return await prismaClient.user.create({ data });
+  }
+
+  async findByEmail(email: string) {
+    return await prismaClient.user.findUnique({ where: { email } });
+  }
+}
