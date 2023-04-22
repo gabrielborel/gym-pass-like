2038f86631424ae54b8299e81946ae8f796a26a2
Gabriel Borel <biel_borel@hotmail.com>
2023-04-22 20:17:46 -0300

    feat: implemented remaining repositories

--- src/repositories/implementations/prisma-check-ins.repository.ts
+++ src/repositories/implementations/prisma-check-ins.repository.ts
@@ -8,4 +8,57 @@ export class PrismaCheckInsRepository implements ICheckInsRepository {
       data,
     });
   }
+
+  async findById(id: string): Promise<CheckIn | null> {
+    return await prismaClient.checkIn.findUnique({ where: { id }});
+  }
+
+  async save(data: CheckIn): Promise<CheckIn> {
+    return await prismaClient.checkIn.update({
+      where: {
+        id: data.id
+      },
+      data
+    });
+  }
+
+  async countByUserId(userId: string): Promise<number> {
+    return await prismaClient.checkIn.count({
+      where: {
+        user_id: userId
+      }
+    });
+  }
+
+  async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    return await prismaClient.checkIn.findMany({
+      where: {
+        user_id: userId,
+      },
+      take: 20,
+      skip: (page - 1) * 20
+    })
+  }
+
+  async findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null> {
+    const startOfDay = new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDay(),
+    ); 
+    const endOfDay = new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDay() + 1
+    );
+    return await prismaClient.checkIn.findFirst({
+      where: {
+        user_id: userId,
+        created_at: {
+          gte: startOfDay,
+          lte: endOfDay
+        }
+      }
+    });
+  }
 }
---
+++ src/repositories/implementations/prisma-gyms.repository.ts
@@ -0,0 +1,38 @@
+import { prismaClient } from "@/lib/prisma";
+import { Prisma, Gym } from "@prisma/client";
+import { IFindManyNearbyParams, IGymsRepository } from "../gyms-repository.interface";
+
+export class PrismaGymsRepository implements IGymsRepository {
+  async create(data: Prisma.GymCreateInput): Promise<Gym> {
+    return await prismaClient.gym.create({
+      data,
+    });
+  }
+
+  async findById(id: string): Promise<Gym | null> {
+    return await prismaClient.gym.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async searchMany(query: string, page: number): Promise<Gym[]> {
+    return await prismaClient.gym.findMany({
+      where: {
+        name: {
+          contains: query
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+  }
+
+  async findManyNearby({ latitude, longitude }: IFindManyNearbyParams): Promise<Gym[]> {
+    return await prismaClient.$queryRaw<Gym[]>`
+      SELECT * FROM gyms
+      WHERE ( 6371 * acos( cos ( radians(${latitude}) ) * cos( radians( latitude )) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) )
+    `; 
+  }
+}
